752890040102eb92bd8ab418f61115309b6d0921
Lidiiaa <lidakulagina@gmail.com>
2020-08-22 07:45:26 +0100

    22.08

--- customJS.js
+++ customJS.js
@@ -1,16 +1,41 @@
-$(function() {
-    
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
+// $(document).ready(function(){
+//   $(window).scroll(function(){
+//     var scroll = $(window).scrollTop();
+//     if (scroll > 300) {
+//       $(".navbar-scroll").css("background" , rgba(52, 58, 64, 0.5);
+//     }
+
+//     else{
+//       $(".navbar-scroll").css("background" , "#333");   
+//     }
+//   })
+// })
+
+/* --------------- Text desappear --------------------*/
+// var x = getElementsByClassName('hoveralim');
+// x.onmouseover = function(Hide){
+//   document.getElementById('disappear1').style.color = "white";
+// };
+
+
+
+//Change pos/background/padding/add shadow on nav when scroll event happens 
+$(function(){
+  var navbar = $('.navbar');
+  
+  $(window).scroll(function(){
+    if($(window).scrollTop() <= 500){
+      navbar.removeClass('nav-dropdown-scroll');
+    } else {
+      navbar.addClass('nav-dropdown-scroll');
     }
   });
-  
+});
+
+/*------------------- HOVER ANIMATION SECTION 3-------------------*/
+
+$('.hovereffect').mouseover(function() {
+    $('#alim-hide').css("color","transparent");;
+}).mouseout(function() {
+    $('#alim-hide').css("color","black");;
 });
